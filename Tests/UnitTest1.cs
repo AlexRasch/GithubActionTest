4929607ce38fd3c3c2d669891afd03571f958aaf
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-07-19 09:15:57 +0200

    added a extra amazing test

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -7,5 +7,11 @@ namespace Tests
         {
             Assert.Equal(2, Domain.Calc.Sum(1,1));
         }
+
+        [Fact]
+        public void Sum_1_and_1__not_equal_1()
+        {
+            Assert.NotEqual(2, Domain.Calc.Sum(1, 1));
+        }
     }
 }
